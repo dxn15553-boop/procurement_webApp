e6fdcf31b4ba05d03fc89005e42ff285bce4551f
Layasri <layasriireni@gmail.com>
2026-07-11 17:01:08 +0530

    Fix Prisma Client initialization for Postgres

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { PrismaLibSql } = require("@prisma/adapter-libsql");
-
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 function createPrismaClient() {
-  const dbUrl = process.env.DATABASE_URL ?? "file:D:/DXN/procurement/prisma/dev.db";
-  const adapter = new PrismaLibSql({ url: dbUrl });
-  return new PrismaClient({ adapter, log: ["error"] });
+  return new PrismaClient({ log: ["error"] });
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
